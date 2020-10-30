3a1f73bd876bda00d7e7b8342d9f0a16d9fa36f7
Wilson Cazarré <wcs0486@gmail.com>
2020-10-30 03:17:28 -0300

    feat: added str methods to models

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from main.models import User, Room, Profile, Nominee, Indication, Category
+from main.models import Room, Profile, Nominee, Indication, Category
 
 # Register your models here.
 
