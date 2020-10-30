dd67570be6cdd326a54b51217089b15e507cd5d9
Wilson Cazarré <wcs0486@gmail.com>
2020-10-30 02:50:10 -0300

    feat: initial data modeling

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from main.models import User, Room, Profile, Nominee, Indication, Category
 
 # Register your models here.
+
+admin.site.register([Room, Profile, Nominee, Indication, Category])
--- main/models.py
+++ main/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+import uuid
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=14)
+    date_joined = models.DateField(auto_now_add=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/')
+
+
+class Nominee(models.Model):
+    name = models.TextField()
+    picture = models.ImageField()
+
+    class Meta:
+        verbose_name_plural = 'nominees'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+
+class Indication(models.Model):
+    nominated = models.ForeignKey(Nominee, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    year = models.DateField()
+    is_winner = models.BooleanField(default=False)
+
+
+class Bet(models.Model):
+    indication = models.ForeignKey(Indication, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+
+class Room(models.Model):
+    share_code = models.CharField(unique=True, max_length=6)
+    name = models.CharField(max_length=14)
+    profile_owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        share_code = self.share_code
+        if not share_code:
+            share_code = uuid.uuid4().hex[:6].upper()
+        while Room.objects.filter(share_code=share_code).exclude(pk=self.pk).exists():
+            share_code = uuid.uuid4().hex[:6].upper()
+        self.systemCode = share_code
+        super().save(*args, **kwargs)
+
+
+class JoinedRoom(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
