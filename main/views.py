c84311c0eb66963e6fe898d618401dc91ab19f86
WilsonCazarre <wcs0486@gmail.com>
2020-11-05 11:41:12 -0300

    feat: added viewsets from django rest framework

--- main/views.py
+++ main/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from main import serializers
+from main import models
+from rest_framework import filters
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = models.UserProfile.objects.all()
+    serializer_class = serializers.UserSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['username', 'email', 'score']
+
+
+class IndicationViewSet(viewsets.ModelViewSet):
+    queryset = models.Indication.objects.all()
+    serializer_class = serializers.IndicationSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+
+class NomineeViewSet(viewsets.ModelViewSet):
+    queryset = models.Nominee.objects.all()
+    serializer_class = serializers.NomineeSerializer
