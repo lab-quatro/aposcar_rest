b79f41631ca9e73dc507cdf78e807b9f00b63156
WilsonCazarre <wcs0486@gmail.com>
2020-11-05 23:15:42 -0300

    feat: added drf-yasg urls patterns

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,23 @@
+from django.conf.urls import url
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from main import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Aposcar API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="labqua4tro@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet, basename='userprofile')
@@ -10,5 +27,8 @@ router.register(r'nominees', views.NomineeViewSet)
 router.register(r'rooms', views.RoomViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
