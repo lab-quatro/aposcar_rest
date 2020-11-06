7bd2c9378da8929e6153b7ebec85fb36cd216cd0
WilsonCazarre <wcs0486@gmail.com>
2020-11-06 01:39:22 -0300

    fix: creating password field correctly

--- main/serializers.py
+++ main/serializers.py
@@ -21,12 +21,22 @@ class IndicationSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    bets = serializers.PrimaryKeyRelatedField(many=True, queryset=models.Indication.objects.all(), required=False)
+    bets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=models.Indication.objects.all(), required=False
+    )
     score = serializers.SerializerMethodField()
 
     class Meta:
         model = models.UserProfile
-        fields = ['url', 'id', 'username', 'email', 'date_joined', 'profile_picture', 'bets', 'score']
+        fields = ['url', 'id', 'username', 'email', 'date_joined',
+                  'profile_picture', 'bets', 'score', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
     def get_score(self, obj):
         return obj.bets.filter(is_winner=True).count()
@@ -37,12 +47,16 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         # Convert to set to remove the duplicates,
         # and check if the list len is still the same
         if len(categories) != len(set(categories)):
-            raise serializers.ValidationError("You can't place two bets to the same category!")
+            raise serializers.ValidationError(
+                "You can't place two bets to the same category!"
+            )
         return value
 
 
 class RoomSerializer(serializers.HyperlinkedModelSerializer):
-    owner = serializers.HyperlinkedRelatedField(read_only=True, view_name='userprofile-detail')
+    owner = serializers.HyperlinkedRelatedField(
+        read_only=True, view_name='userprofile-detail'
+    )
 
     class Meta:
         model = models.Room
