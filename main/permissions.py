b309d1514937207abeabe751ad2295f1d0197ca9
WilsonCazarre <wcs0486@gmail.com>
2020-11-06 02:36:34 -0300

    feat: add permissions to check staff status

--- main/permissions.py
+++ main/permissions.py
@@ -10,3 +10,22 @@ class IsOwnerOrInRoom(permissions.BasePermission):
 
         # Write permission are only allowed if the user is the room owner.
         return obj.owner == request.user
+
+
+class IsProfileOwnerOrReadOnlyOrStaff(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user == obj or request.user.is_staff
+
+
+class IsStaffOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_staff
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_staff
