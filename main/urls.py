b66643b1660abf5cfc0067d546877b0413a1dacb
WilsonCazarre <wcs0486@gmail.com>
2020-11-05 22:41:55 -0300

    feat: added rooms endpoint

--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,11 @@ from rest_framework.routers import DefaultRouter
 from main import views
 
 router = DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register(r'users', views.UserViewSet, basename='userprofile')
 router.register(r'indications', views.IndicationViewSet)
 router.register(r'categories', views.CategoryViewSet)
 router.register(r'nominees', views.NomineeViewSet)
+router.register(r'rooms', views.RoomViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
