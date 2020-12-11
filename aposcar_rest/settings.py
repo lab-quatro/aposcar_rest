d6d973bd6405b50cca9b93d531a5506501840622
WilsonCazarre <wcs0486@gmail.com>
2020-12-11 15:02:51 -0300

    feat: disable debug

--- aposcar_rest/settings.py
+++ aposcar_rest/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['aposcar.herokuapp.com', 'localhost:3000']
 
