0f0f0a18a3b01ae65e73032b8d3acebf93a6cc08
WilsonCazarre <wcs0486@gmail.com>
2021-02-13 12:07:17 -0300

    feat: add basic authentication to the api

--- aposcar_rest/urls.py
+++ aposcar_rest/urls.py
@@ -7,5 +7,6 @@ from rest_framework.authtoken import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.core.urls')),
+    path('api-auth', include('rest_framework.urls')),
     path('login/', views.obtain_auth_token)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
